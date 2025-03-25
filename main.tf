5bfd8a2b84c142a1172828cc4c9111f1c77cae4f
Matvey <matvevait5686@gmail.com>
2025-03-25 04:19:10 +0200

    initial commit

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = data.aws_vpc.default.id
-
   ingress {
     description = "SSH from anywhere"
     from_port   = 22
@@ -25,22 +24,25 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = {
     Name = "allow_ssh"
   }
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "ed25519_tf_managed"
-  public_key = file("~/.ssh/win_ed25519.pub")
+  key_name   = "ssh_key_tf_managed"
+  public_key = var.ssh_public_key
+}
+
+variable "ssh_public_key" {
+  type    = string
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHWGgCeR/N5uh/t5zLtPdNUbyhS6uXMZf4V0JcA/CGZt matvey-key"
 }
 
 resource "aws_instance" "example" {
@@ -53,7 +55,6 @@ resource "aws_instance" "example" {
     Name = "postgres"
   }
 }
-
 output "public_ip" {
   value = aws_instance.example.public_ip
 }
